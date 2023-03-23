b6627ab5c01aca343ef505241ba946838da0d0ff
dima9917 <41657345+dima9917@users.noreply.github.com>
2023-03-22 17:13:06 -0700

    sdfsd

---
+++ test/ToDoAPI.Tests/ToDoModelTests.cs
@@ -0,0 +1,18 @@
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Tests
+{
+    public class ToDoModelTests
+    {
+        [Fact]
+        public void CanChangeName()
+        {
+            //Arrange
+            var testToDo = new ToDoItem { Name = "1" };
+            //Act
+            testToDo.Name = "2";
+            //Assert
+            Assert.Equal(testToDo.Name, "2");
+        }
+    }
+}
